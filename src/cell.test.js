c8734a4bf8f96c7b8c25bc0cd4002c925b30557a
Weslley Silva <weslley_ssilva@hotmail.com>
2016-10-15 15:44:56 -0300

    teste onToggle

--- src/cell.test.js
+++ src/cell.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import Cell from './cell';
 
 describe('Cell', () => {
@@ -10,10 +11,20 @@ describe('Cell', () => {
 
       expect(wrapper.hasClass('cell')).to.equal(true);
     });
+
     it('Has a class indicating it is alive', () => {
       const wrapper = shallow(<Cell isAlive onToggle={() => {}} />);
 
       expect(wrapper.hasClass('cell--alive')).to.equal(true);
+    });
+
+    it('Has called onToggle', () => {
+      const onToggle = sinon.spy();
+      const wrapper = shallow(<Cell isAlive={false} onToggle={onToggle} />);
+
+      wrapper.simulate('click');
+
+      expect(onToggle.calledOnce).to.equal(true);
     });
   });
 
